8fe041fd14fb00b5509fafed43add746d9498450
tommy-021 <tommy.brtek@partnersbanka.cz>
2025-08-21 19:34:12 +0200

    - data

---
+++ client/src/components/Event.tsx
@@ -0,0 +1,17 @@
+export type UserRecord = {
+    name: string;
+    answer: "yes" | "no" | "if-needed";
+};
+
+export type DateRecord = {
+    timestamp: number;
+    records: UserRecord[];
+};
+
+// props komponenty Event
+export type EventProps = {
+    location?: string;
+    id: string;
+    title: string;
+    dates: DateRecord[];
+};
---
+++ client/src/data.ts
@@ -0,0 +1,22 @@
+type Events = {
+    location?: string;
+    id: string;
+    title: string;
+};
+export const events: Events[] = [
+    {
+        location: 'Praha',
+        id: '1',
+        title: 'jedeme'
+    },
+    {
+        location: 'Brno',
+        id: '2',
+        title: 'jedeme skoro'
+    },
+    {
+        location: 'Ostrava',
+        id: '3',
+        title: 'nejde'
+    }
+];
